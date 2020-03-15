c9e2f79f6558c068a5ab6018051154951743a553
diego <diegobarrios.c@gmail.com>
2020-03-15 13:10:16 +0000

    add

--- front-end/server.js
+++ front-end/server.js
@@ -45,7 +45,6 @@ app.use(cart);
 app.use(catalogue);
 app.use(orders);
 app.use(user);
-// app.use(upload);
 var server = app.listen(process.env.PORT || 8079, function () {
   var port = server.address().port;
   console.log("App now running in %s mode on port %d", app.get("env"), port);
